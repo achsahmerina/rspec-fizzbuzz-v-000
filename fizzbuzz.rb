6143d834e023c85fd2051acff704c751022a3d97
Achsah Abraham <achsahmp@gmail.com>
2018-06-06 23:12:39 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 def fizzbuzz(number)
-  if (number % 3 == 0) && (number % 5 == 0)
+  if number % 3 == 0 && number % 5 == 0
     "FizzBuzz"
   elsif number % 5 == 0
     "Buzz"
   elsif number % 3 == 0
-    "Fizz" 
+    "Fizz"
 end
